c75e01ae51076fd6513bc5681c162917283518a5
yin liu <yinnie.yinnie@gmail.com>
2019-11-14 23:04:42 -0500

    Created components. parse real LevelNode Json data.

--- src/index.js
+++ src/index.js
@@ -3,38 +3,63 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import data from './data/testDots';
 
-class Board extends React.Component {
+const levelNodeJson = data;
+const levelData = JSON.parse(data.level_blob);
+
+class Dot extends React.Component {
   render() {
-    const dotsData = data.dots;
-    const width = data.width;
+    return <div className='dot'>{this.props.dotType}</div>;
+  }
+}
 
-    const dotsLayout = GetDotsLayout(dotsData, width);
+class Board extends React.Component {
+  renderDots() {
+    const dotsOneD = levelData.dots;
+    const numberDotsPerRow = levelData.width;
+    const dotsTwoD = GetDotsInTwoDimensionalArray(dotsOneD, numberDotsPerRow);
 
+    return <table>{dotsTwoD.map((row, index) => this.renderRow(row))}</table>;
+  }
+
+  renderRow(rowData) {
     return (
-      <table>
-        {dotsLayout.map((row, index) => (
-          <tr>
-            {row.map(dot => (
-              <td>{dot}</td>
-            ))}
-          </tr>
+      <tr>
+        {rowData.map(dot => (
+          <td>{this.renderSingleDot(dot)}</td>
         ))}
-      </table>
+      </tr>
+    );
+  }
+
+  renderSingleDot(dotType) {
+    return <Dot dotType={dotType} />;
+  }
+
+  render() {
+    return (
+      <div className='Board'>
+        <div className='BoardHeading'>
+          <p>Gloal Level Number: {levelNodeJson.global_level_number}</p>
+          <p>World Number: {levelData.world_number}</p>
+          <p>Moves: {levelData.limiter_amount}</p>
+        </div>
+        <div classname='BoardGrid'>{this.renderDots()}</div>
+      </div>
     );
   }
 }
 
-function GetDotsLayout(dotsDataFromJson, numberOfDotsPerRow) {
-  let doubleArray = new Array();
+function GetDotsInTwoDimensionalArray(dotsDataFromJson, numberOfDotsPerRow) {
+  let twoDimensionArray = [];
+  let indexInArray = 0;
 
   for (let i = 0; i < dotsDataFromJson.length; i += numberOfDotsPerRow) {
-    let rowData = dotsDataFromJson.slice(i, numberOfDotsPerRow);
-    doubleArray.push(rowData);
-
-    alert('one row');
+    let rowData = dotsDataFromJson.slice(i, i + numberOfDotsPerRow);
+    twoDimensionArray[indexInArray] = rowData;
+    indexInArray++;
   }
 
-  return doubleArray;
+  return twoDimensionArray;
 }
 
 // ========================================
